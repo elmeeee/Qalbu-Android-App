7437ba5c4bf89545af4479fdf60551f450b6be2e
elmeeee <37185272+elmeeee@users.noreply.github.com>
2026-08-04 16:44:02 +0700

    fix: increment versionCode to 7 for the upcoming release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,7 +31,7 @@ android {
         applicationId = "app.kamy.saatApp"
         minSdk = 30
         targetSdk = 36
-        versionCode = 6
+        versionCode = 7
         versionName = "1.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
