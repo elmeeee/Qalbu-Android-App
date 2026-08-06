be733434a3042dc38c4295b74a030bfcbfcf6225
elmeeee <37185272+elmeeee@users.noreply.github.com>
2026-08-06 16:10:43 +0700

    chore: bump app version to 1.2.0 and increment version code to 8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -31,8 +31,8 @@ android {
         applicationId = "app.kamy.saatApp"
         minSdk = 30
         targetSdk = 36
-        versionCode = 7
-        versionName = "1.1.1"
+        versionCode = 8
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
